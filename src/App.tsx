c14f5cd8a56c9c78d248748031f70d884222be22
hungdn <hungdn@runsystem.net>
2022-05-26 17:04:08 +0700

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    document.title = "v18";
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      bg="#e9f3fe"
+      w="100vw"
+      h="100vh"
+    >
+      <Box
+        mt="-16rem"
+        w="107.5rem"
+        h="38rem"
+        bg="white"
+        rounded="5rem"
+        p="5.2rem"
+      >
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          flexDirection="column"
+          border="4px dashed #CEDAE6"
+          w="100%"
+          h="100%"
+          rounded="2.4rem"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Image src="" />
+
+          <Box>
+            <Text display="inline-block">Drop your file here, or</Text>
+            <Text display="inline-block">browse</Text>
+          </Box>
+          <Box>
+            <Text w="fit-content">Supports: All</Text>
+          </Box>
+        </Flex>
+      </Box>
+    </Flex>
   );
 }
 
