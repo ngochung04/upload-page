7fa811bc3f1bf8a8672548a227c1302989c2a9bd
hungdn <hungdn@runsystem.net>
2022-06-01 16:49:16 +0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, LinkOverlay, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import "./App.css";
+
+interface Res {
+  error: boolean;
+  message: string;
+  status: string;
+  url?: string;
+}
 
 function App() {
   useEffect(() => {
     document.title = "v18";
   }, []);
 
-  const { acceptedFiles, getRootProps, getInputProps, rootRef } = useDropzone();
-  const [response, setResponse] = useState<any>(null);
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+  const [response, setResponse] = useState<Res>();
   const callApi = useCallback(async () => {
     const formData = new FormData();
     acceptedFiles && formData.append("file", acceptedFiles[0]);
@@ -29,9 +36,10 @@ function App() {
     }
   }, []);
 
-  const handleSend = () => {
+  const handleSend = useCallback(() => {
     if (acceptedFiles.length > 0) callApi();
-  };
+    console.log(acceptedFiles[0]);
+  }, [acceptedFiles]);
 
   return (
     <Flex
@@ -60,42 +68,84 @@ function App() {
           h="100%"
           rounded="2.4rem"
         >
-          <Box>
-            <Text display="inline-block" fontSize="2.4rem" fontWeight="bold">
-              Drop your file here, or
-            </Text>
-            <Text
-              ml="8px"
-              display="inline-block"
-              fontSize="2.4rem"
-              fontWeight="bold"
-              color="#0E86D4"
-            >
-              browse
-            </Text>
-          </Box>
-          <Text
-            mx="auto"
-            transform="translateY(-5px)"
-            w="fit-content"
-            fontSize="2rem"
-            fontWeight="medium"
-          >
-            Supports: All
-          </Text>
-
-          <Button
-            onClick={handleSend}
-            color="#0E86D4"
-            mt="20px"
-            p="20px 28px"
-            fontSize="2rem"
-            fontWeight="medium"
-          >
-            upload
-          </Button>
+          {acceptedFiles.length > 0 ? (
+            <>
+              <Text display="inline-block" fontSize="2.4rem" fontWeight="bold">
+                {acceptedFiles[0].name}
+              </Text>
+              <Text
+                mx="auto"
+                transform="translateY(-5px)"
+                w="fit-content"
+                fontSize="2rem"
+                fontWeight="medium"
+              >
+                Size: {(acceptedFiles[0].size / 1024 / 1024).toFixed(2)} MB
+              </Text>
+              {response?.error === false && (
+                <>
+                  <Text fontSize="2.4rem" fontWeight="bold" color="#32CD32">
+                    Upload Success
+                  </Text>
+                  <LinkOverlay
+                    href={response?.url}
+                    onClick={(e) => e.stopPropagation()}
+                  >
+                    <Text color="blue" fontSize="1.4rem" fontWeight="medium">
+                      {response?.url}
+                    </Text>
+                  </LinkOverlay>
+                </>
+              )}
+            </>
+          ) : (
+            <>
+              <Box>
+                <Text
+                  display="inline-block"
+                  fontSize="2.4rem"
+                  fontWeight="bold"
+                >
+                  Drop your file here, or
+                </Text>
+                <Text
+                  ml="8px"
+                  display="inline-block"
+                  fontSize="2.4rem"
+                  fontWeight="bold"
+                  color="#0E86D4"
+                >
+                  browse
+                </Text>
+              </Box>
+              <Text
+                mx="auto"
+                transform="translateY(-5px)"
+                w="fit-content"
+                fontSize="2rem"
+                fontWeight="medium"
+              >
+                Supports: All
+              </Text>
+            </>
+          )}
         </Flex>
-        {response}
+        <Button
+          hidden={acceptedFiles.length === 0}
+          onClick={handleSend}
+          color="#FFF"
+          bg="#0E86D4"
+          mt="20px"
+          left="50%"
+          transform="translate(-50%)"
+          w="fit-content"
+          p="28px 36px"
+          rounded="full"
+          fontSize="2rem"
+          fontWeight="BOLD"
+        >
+          UPLOAD
+        </Button>
         <input type="file" {...getInputProps()} />
       </Box>
     </Flex>
